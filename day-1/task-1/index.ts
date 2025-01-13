9e4865a207bda4f56886654b556cd3f2b2e47068
Рина <133646056+Sauky13@users.noreply.github.com>
2025-01-13 17:01:03 +0700

    Day 1
    поправила 1 задание

--- day-1/task-1/index.ts
+++ day-1/task-1/index.ts
@@ -1,15 +1,11 @@
-function identicalLetters(str1: string, str2: string) {
+function identicalLetters(str1: string, str2: string): string {
 
-  console.log(str1, str2 );
- 
-   const set1 = new Set(str1);
-   const set2 = new Set(str2);
- 
-   const commonLetters = [...set1].filter(function (letter) {
-     return set2.has(letter);
-   });
- 
-   return commonLetters.join("");
- }
- 
- console.log(identicalLetters("Варенье", "Венок"));
+  const commonLetters = str1.split('').filter(function (char) {
+      return str2.includes(char);
+  });
+
+
+  return commonLetters.join('');
+}
+
+console.log(identicalLetters("asdffg", "asdqffwe"));
